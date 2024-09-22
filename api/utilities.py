cc0f08ca7c4dbe5faf9b2f71070dfaf06d032b9f
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-09-22 12:42:33 +0100

    LoginId Generation Function

---
+++ api/utilities.py
@@ -0,0 +1,22 @@
+import random
+import  string
+from api.models import User
+
+
+def generate_loginid():
+    used_numbers = set()
+
+    while True:
+        letters = "".join(random.choices(string.ascii_uppercase, k=3))
+        number = random.randint(0, 999)
+        login_id = f"{letters}-{number:03d}"
+
+        if not login_id_exist(login_id) and number not in used_numbers:
+            used_numbers.add(number)
+            return login_id
+
+        if len(used_numbers) == 1000:
+            used_numbers.clear()
+
+def login_id_exist(login_id):
+   return User.objects.filter(login_id=login_id).exists()
