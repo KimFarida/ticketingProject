253ebdcb18932c18c60bf00b304abfbadec8c76d
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-10-11 12:39:14 +0100

    Revert "Clean up"
    This reverts commit 794e0c64d6029677fac9c8da68f84ce6b3608f69.

--- api/agent/views.py
+++ api/agent/views.py
@@ -2,7 +2,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from api.models import Merchant,  Wallet
+from api.models import Merchant, Agent, Wallet
+from .serializer import MerchantBalanceSerializer  #, AgentCreateVoucherSerializer, VoucherDetailSerializer
+from ..account.permissions import IsAgent
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 @api_view(['GET'])
