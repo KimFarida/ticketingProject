8bb03e49087069394d31e63f3413e22500c5a51a
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-10-14 13:37:53 +0100

    Test User

--- ticketing_api/settings.py
+++ ticketing_api/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         #'rest_framework.permissions.IsAuthenticated',
     )
--- ticketing_api/urls.py
+++ ticketing_api/urls.py
@@ -28,7 +28,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
-    url='https://profitplay9ja.com.ng'
+    #url='https://profitplay9ja.com.ng'
 )
 
 urlpatterns = [
