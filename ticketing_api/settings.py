aaa7589ba0f85ffe843c22d83c7dbe3c1d597b90
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-10-14 12:41:42 +0100

    Added Domain name to settings.py

--- ticketing_api/settings.py
+++ ticketing_api/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['102.130.118.149', 'vm659smii.yourlocaldomain.com', '127.0.0.1']
+ALLOWED_HOSTS = ['102.130.118.149', 'profitplay9ja.com.ng', 'www.profitplay9ja.com.ng']
 
 
 # Application definition
