b53a4d71aa2b2085e7ada3d38f998efa53922412
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-10-21 18:16:33 +0100

    Refactor: Chnaged issued vouchers to sold vouchers

--- api/voucher/urls.py
+++ api/voucher/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import created_vouchers, issued_vouchers, create_voucher, process_voucher, get_voucher
+from .views import sold_vouchers, issued_vouchers, create_voucher, process_voucher, get_voucher
 
 
 urlpatterns = [
-    path('created_vouchers/', created_vouchers, name='created_vouchers'),
+    path('sold_vouchers/', sold_vouchers, name='sold_vouchers'),
     path('issued_vouchers/', issued_vouchers, name='issued_vouchers'),
     path('create_voucher/', create_voucher, name='create_voucher'),
     path('get_voucher/', get_voucher, name='get_voucher'),
