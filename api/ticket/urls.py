998614a5514ec7818ec1b9b58efb6c7e5bb40ff7
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-10-24 10:25:28 +0100

    Fix: Corrected Path Param (Update & Delete Ticket Type)

--- api/ticket/urls.py
+++ api/ticket/urls.py
@@ -4,8 +4,8 @@ from .views import create_ticket_type,list_ticket_types,update_ticket_type, dele
 urlpatterns = [
     path("ticket-type/", create_ticket_type, name="create_ticket_type"),
     path("ticket-types/list/", list_ticket_types, name="list_ticket_types"),
-    path("ticket_type/<int:pk>/", update_ticket_type, name="update_ticket_type"),
-    path("ticket-type/<int:pk>/delete/", delete_ticket_type, name="delete_ticket_type"),
+    path("ticket_type/<str:id>/", update_ticket_type, name="update_ticket_type"),
+    path("ticket-type/<str:id>/delete/", delete_ticket_type, name="delete_ticket_type"),
     path("create-ticket/", create_tickets, name="create_tickets"),
     path("check-ticket/<str:ticket_code>/", check_ticket_validity, name="check_ticket_validity"),
     path("get-agent-tickets/", get_agent_tickets, name="get_agent_tickets"),
