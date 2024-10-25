e671f71e04359658cc121e650dfda1190358524f
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-10-25 11:57:39 +0100

    Fix:API URL

--- FRONTEND/src/api/axios.ts
+++ FRONTEND/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.PROD
-        ? 'https://profitplay9ja.com.ng/api'  // Production URL
+        ? 'https://profitplay9ja.com.ng/'  // Production URL
         : '',                              // Development URL (for Vite proxy)
     headers: {
         'Content-Type': 'application/json'
