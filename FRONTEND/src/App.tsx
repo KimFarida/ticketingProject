6b582322a16b385fb43aed155625e0231ea3b4ec
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-10-30 17:07:43 +0100

    Reafctor:Removed Redundant Import

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import SignUpPage from "./pages/signUpPage";
 import LandingPage from "./pages/landingPage";
