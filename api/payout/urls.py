bbc1da0d1622f32cbde0dfd22619cfb53a6a7835
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-10-30 15:59:54 +0100

    Fix:added str formant to endpoint url

--- api/payout/urls.py
+++ api/payout/urls.py
@@ -5,7 +5,7 @@ from .views import request_payout, list_payout_requests, process_payout, get_pay
 urlpatterns = [
     path('request/', request_payout, name='payout'),
     path('list/', list_payout_requests, name='payout_requests'),
-    path('get_payout_by_id/', get_payout_by_id, name='payout_by_id'),
+    path('get_payout_by_id/<str:payment_id>', get_payout_by_id, name='payout_by_id'),
     path('process/<str:payment_id>', process_payout, name='payout_process')
 
 ]
