651d260d8a9248f0f3c87440098aee172bfdcf7a
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-11-02 16:39:59 +0100

    Fix: Swagger Reflection ii

--- api/admin/urls.py
+++ api/admin/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import promote_to_merchant, list_merchants, list_agents, ticket_sales_log
+from .views import promote_to_merchant, list_merchants, list_agents, ticket_sales_log, update_payout_settings
 
 urlpatterns = [
     path('promote-to-merchant/<uuid:user_id>/', promote_to_merchant, name='promote_to_merchant'),
     path('merchants/', list_merchants, name='list_merchants'),
     path('agents/', list_agents, name='list_agents'),
     path('ticket-sales-log/', ticket_sales_log, name='ticket_sales_log'),
+    path('update-payout-settings/', update_payout_settings, name='update_payout_settings'),
+
 ]
