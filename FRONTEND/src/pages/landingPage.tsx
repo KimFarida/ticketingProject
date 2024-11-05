48b664f84bd46d9660fbc4c009be8669533e6a10
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-11-05 23:49:18 +0100

    Refactor:Moduralized NavBar\n Fix: Aligned Footer and added Social Media Links

--- FRONTEND/src/pages/landingPage.tsx
+++ FRONTEND/src/pages/landingPage.tsx
@@ -1,50 +1,18 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './signUp.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faBars, faTimes} from '@fortawesome/free-solid-svg-icons'
 import petImage from '../images/profit play.png'
-import AppLogo from '../images/profitplay-black.png'
-import TextCarousel from '../components/textCarousel';
+import NavBar from "@/components/navBar.tsx";
 import ScrollAnimation from '../components/logoAnimation';
 import TicketValidator from '../components/ticketValidator'
 
 
 
 function LandingPage(){
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const onToggleMenu = () => {
-        console.log('Menu toggled');
-        
-        setIsMenuOpen(!isMenuOpen);
-    };
 
     return(
         <>
             <header className="bg-[#000000]">
-                <nav className="flex justify-between items-center w-[92%]  mx-auto">
-                    <div>
-                    <img src={AppLogo} alt="App Logo" className="w-24 " />
-                    </div>
-                    <div className={`md:static absolute md:min-h-fit min-h-[60vh] left-0 ${ isMenuOpen ? 'top-[9%] w-full bg-[#000000] z-50' : 'top-[-100%] w-full'} md:w-auto w-full flex items-center px-5`}>
-                        <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 text-white">
-                            <li><Link to='/'><a>Home</a></Link></li>
-                            {/*<li><a href="#"></a>Become an Agent</li>*/}
-                            <li><a href="#">Contact Us</a></li>
-                            <li><Link to='/about-us'><a>About Us</a></Link></li>
-                        </ul>
-                        
-                    </div>
-                    <div className='flex items-center gap-3 py-2'>
-                        <Link to="/signin" className='border-2 px-2 border-[#6AE803] ml-2 rounded text-white whitespace-nowrap'>
-                            <button>LOG IN</button>
-                        </Link>
-                        <button><Link to="/signup" className="bg-[#6AE803] text-white px-5 py-2 rounded-full hover:bg-[#58D106] whitespace-nowrap"> Sign Up</Link></button>
-                        <FontAwesomeIcon icon= {isMenuOpen ? faTimes : faBars} className='text-3xl text-white cursor-pointer md:hidden' onClick={onToggleMenu} />
-                    </div>
-                </nav>
-
+                <NavBar></NavBar>
                 {/* HERO SECTION */}
                 <div className="m-20 flex flex-col justify-center items-center text-center px-4 ">
                     <div className="relative z-10 text-white px-4">
@@ -71,72 +39,99 @@ function LandingPage(){
             {/* EVENTS SECTION */}
             <div className="m-15 container mx-auto py-10 flex flex-col justify-center items-center text-center px-4">
                 <div className="relative z-10 px-4">
-                    
+
                     <h2 className="text-2xl md:text-3xl lg:text-3xl italic mb-4 max-w-full">
-                    There is something here for everyone
+                        There is something here for everyone
                     </h2>
 
-                    
+
                     <h6 className="text-sm text-gray-700 italic md:text-xl lg:text-xl mb-8 mt-8 max-w-full overflow-hidden">
                         Play with purpose, win with passion
                     </h6>
 
-                    <div className="flex flex-col md:flex-row items-center justify-between m-10">  
+                    <div className="flex flex-col md:flex-row items-center justify-between m-10">
                         <div className="md:w-1/2 text-left p-4">
                             <h4 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
-                            Immerse yourself in exciting gameplay, expertly crafted for ultimate enjoyment.
+                                Immerse yourself in exciting gameplay, expertly crafted for ultimate enjoyment.
                             </h4>
 
                             <p className="text-base md:text-lg lg:text-xl mb-8">
                                 Experience thrilling games, expertly designed for maximum reward
                             </p>
-                            <Link to="/signup">
-                                <button className="bg-[#607714] text-white py-2 px-4 sm:py-3 sm:px-8 rounded-full hover:bg-orange-600 transition duration-300">
-                                Join the Profit Play family!
+                            <Link to={"/signup"}>
+                                <button
+                                    className="bg-[#607714] text-white py-2 px-4 sm:py-3 sm:px-8 rounded-full hover:bg-orange-600 transition duration-300">
+                                    Join the Profit Play family!
                                 </button>
                             </Link>
-                            
+
                         </div>
                         <div className="md:w-1/2 flex justify-center md:justify-end mt-4 md:mt-0 p-4">
-                                <img src={petImage} alt="Description" className="w-full h-auto rounded-lg" />
+                            <img src={petImage} alt="Description" className="w-full h-auto rounded-lg"/>
                         </div>
                     </div>
 
                 </div>
             </div>
-            <TextCarousel/>
             <ScrollAnimation/>
 
             {/* FOOTER */}
-            
-            <footer className="mt-20 text-white bg-[#000000]">
-                <div className="m-2">
-                    <h2>Profit Play is an event ticketing platform for memorable experiences in Africa</h2>
-                </div>
-                <div className=" w-full flex justify-center">
-                    <div className="w-full md:w-3/4 lg:w-2/3 p-2 flex flex-start justify-between">
-                        <div>
-                            <p className='font-semibold'>Company</p>
-                            <ul className="text-sm italic grid grid-cols-2 gap-2">
-                                <li className='m-1'>About Us</li>
-                                <li className='m-1'>How it works</li>
-                                <li className='m-1'>Blog</li>
+
+            <footer className="mt-20 bg-black text-white py-8">
+                <div className="container mx-auto px-4">
+                    <div className="max-w-4xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-8">
+                        {/* Company Section */}
+                        <div className="col-span-1">
+                            <h3 className="font-semibold text-lg mb-4">Company</h3>
+                            <ul className="space-y-2">
+                                <li className="hover:text-gray-300 cursor-pointer transition-colors">
+                                    About Us
+                                </li>
+                                <li className="hover:text-gray-300 cursor-pointer transition-colors">
+                                    How it works
+                                </li>
+                                <li className="hover:text-gray-300 cursor-pointer transition-colors">
+                                    Blog
+                                </li>
                             </ul>
                         </div>
-                        <div>
-                            <p className='font-semibold'>Follow Us</p>
-                            <ul className="text-sm italic grid grid-cols-2 gap-2">
-                                <li className='m-1'>Facebook</li>
-                                <li className='m-1'>X(Twitter)</li>
-                                <li className='m-1'>Instagram</li>
-                                <li className='m-1'>Tiktok</li>
+
+                        {/* Follow Us Section */}
+                        <div className="col-span-1">
+                            <h3 className="font-semibold text-lg mb-4">Follow Us</h3>
+                            <ul className="space-y-2">
+                                <li className="hover:text-gray-300 cursor-pointer transition-colors">
+                                    <a href={'https://www.facebook.com/profile.php?id=61567109963352'}
+                                       target={'_blank'}>
+                                        Facebook
+                                    </a>
+                                </li>
+                                <li className="hover:text-gray-300 cursor-pointer transition-colors">
+                                    <a href={'https://x.com/Profitplay9ja'} target={'_blank'}>
+                                        X (Twitter)
+                                    </a>
+                                </li>
+                                <li className="hover:text-gray-300 cursor-pointer transition-colors">
+                                    <a href={'https://www.instagram.com/profitplay9ja/'} target={'_blank'}>
+                                        Instagram
+                                    </a>
+                                </li>
+                                <li className="hover:text-gray-300 cursor-pointer transition-colors">
+                                    <a href={'https://t.me/profitplay9ja'} target={'_blank'}>
+                                        Telegram
+                                    </a>
+                                </li>
+                                <li className="hover:text-gray-300 cursor-pointer transition-colors">
+                                    <a href={'https://www.tiktok.com/@profitplay9ja'} target={'_blank'}>
+                                        TikTok
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     </div>
                 </div>
-
             </footer>
-            
+
         </>
     )
 }
